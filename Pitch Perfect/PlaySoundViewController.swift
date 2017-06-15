07e894441e781241a6a2a028e448c6033b0ce8ae
Javon Davis <javonldavis14@gmail.com>
2017-06-14 20:27:24 -0500

    initial commit with project files

---
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by Javon Davis on 14/06/2017.
+//  Copyright © 2017 Javon Davis. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pitch Perfect/RecordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Javon Davis on 14/06/2017.
+//  Copyright © 2017 Javon Davis. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
